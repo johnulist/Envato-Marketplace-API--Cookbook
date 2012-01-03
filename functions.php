3edbbd4fa2d7ffb41da5132beb89009ccebcbb43
Jeffrey Way <jeffrey@jeffrey-way.com>
2012-01-03 17:27:30 -0500

    Added a touch of security to the proxy thingy

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
-//if ( $_POST['id'] && $_POST['file_name'] ) {
+if ( isset($_POST['href']) && !file_exists($_POST['href']) ) {
 	echo htmlspecialchars(
 		file_get_contents($_POST['href'])
 	);
-//}
+}
